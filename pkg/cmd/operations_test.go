4425372287415fd448ef5028778cc8b7de258bcd
martinohmann <martinohmann@gmail.com>
2021-01-02 16:46:06 +0100

    feat: confirmation prompt for destructive commands
    This guards against accidental deletion and overwrite of existing
    secrets. User can auto-confirm the prompt by passing the `--yes` flag to
    the `set` and `delete` commands.

--- pkg/cmd/operations_test.go
+++ pkg/cmd/operations_test.go
@@ -44,7 +44,7 @@ func TestGetCommand_NotFound(t *testing.T) {
 	require.Error(cmd.Execute())
 }
 
-func TestSetCommand(t *testing.T) {
+func TestSetCommand_Create(t *testing.T) {
 	require := require.New(t)
 
 	keyring.MockInit()
@@ -60,14 +60,57 @@ func TestSetCommand(t *testing.T) {
 	cmd.SetOut(&buf)
 
 	require.NoError(cmd.Execute())
-	require.Equal(secretSavedMsg+"\n", buf.String())
+	require.Contains(buf.String(), secretCreatedMsg)
 
 	secret, err := keyring.Get(testService, testUser)
 	require.NoError(err)
 	require.Equal(testSecret, secret)
 }
 
-func TestDeleteCommand(t *testing.T) {
+func TestSetCommand_UpdateConfirm(t *testing.T) {
+	require := require.New(t)
+
+	keyring.MockInit()
+
+	require.NoError(keyring.Set(testService, testUser, testSecret))
+
+	var buf bytes.Buffer
+
+	cmd := newRootCommand()
+	cmd.SetArgs([]string{"set", testService, testUser, "--yes"})
+	cmd.SetIn(bytes.NewBuffer([]byte(testSecret + "new")))
+	cmd.SetOut(&buf)
+
+	require.NoError(cmd.Execute())
+	require.Contains(buf.String(), secretUpdatedMsg)
+
+	secret, err := keyring.Get(testService, testUser)
+	require.NoError(err)
+	require.Equal(testSecret+"new", secret)
+}
+
+func TestSetCommand_UpdateAbort(t *testing.T) {
+	require := require.New(t)
+
+	keyring.MockInit()
+
+	require.NoError(keyring.Set(testService, testUser, testSecret))
+
+	var buf bytes.Buffer
+
+	cmd := newRootCommand()
+	cmd.SetArgs([]string{"set", testService, testUser})
+	cmd.SetIn(bytes.NewBuffer([]byte(testSecret + "new")))
+	cmd.SetOut(&buf)
+
+	require.Error(cmd.Execute())
+
+	secret, err := keyring.Get(testService, testUser)
+	require.NoError(err)
+	require.Equal(testSecret, secret)
+}
+
+func TestDeleteCommand_Confirm(t *testing.T) {
 	require := require.New(t)
 
 	keyring.MockInit()
@@ -78,10 +121,51 @@ func TestDeleteCommand(t *testing.T) {
 
 	cmd := newRootCommand()
 	cmd.SetArgs([]string{"delete", testService, testUser})
+	cmd.SetIn(bytes.NewBuffer([]byte(`y`)))
 	cmd.SetOut(&buf)
 
 	require.NoError(cmd.Execute())
-	require.Equal(secretDeletedMsg+"\n", buf.String())
+	require.Contains(buf.String(), secretDeletedMsg)
+
+	_, err := keyring.Get(testService, testUser)
+	require.Error(err)
+}
+
+func TestDeleteCommand_Abort(t *testing.T) {
+	require := require.New(t)
+
+	keyring.MockInit()
+
+	require.NoError(keyring.Set(testService, testUser, testSecret))
+
+	var buf bytes.Buffer
+
+	cmd := newRootCommand()
+	cmd.SetArgs([]string{"delete", testService, testUser})
+	cmd.SetIn(bytes.NewBuffer(nil))
+	cmd.SetOut(&buf)
+
+	require.Error(cmd.Execute())
+
+	_, err := keyring.Get(testService, testUser)
+	require.NoError(err)
+}
+
+func TestDeleteCommand_AutoConfirm(t *testing.T) {
+	require := require.New(t)
+
+	keyring.MockInit()
+
+	require.NoError(keyring.Set(testService, testUser, testSecret))
+
+	var buf bytes.Buffer
+
+	cmd := newRootCommand()
+	cmd.SetArgs([]string{"delete", testService, testUser, "--yes"})
+	cmd.SetOut(&buf)
+
+	require.NoError(cmd.Execute())
+	require.Contains(buf.String(), secretDeletedMsg)
 
 	_, err := keyring.Get(testService, testUser)
 	require.Error(err)
